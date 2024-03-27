4494a7d1cb48ec2164f41b2d3736130eab4b0ac6
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-27 13:39:39 +0800

    二分查找 DONE

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1543,3 +1543,31 @@ using namespace std;
 //         return ret;
 //     }
 // };
+
+// 二分查找
+// class Solution 
+// {
+// public:
+//     int search(vector<int>& nums, int target) 
+//     {
+//         int left = 0, right = nums.size() - 1;
+//         while(left <= right)
+//         {
+//             int mid = left + (right - left) / 2; // 防止溢出
+//             if(nums[mid] < target)
+//             {
+//                 left = mid + 1;
+//             }
+//             else if(nums[mid] > target)
+//             {
+//                 right = mid - 1;
+//             }
+//             else
+//             {
+//                 return mid;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// };
