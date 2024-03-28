646b3dcf298a26147505c8e9841ba1870b2e0bfb
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-28 22:37:54 +0800

    mySqrt

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1628,3 +1628,34 @@ using namespace std;
 //         return {begin, right};
 //     }
 // };
+
+// x的平方根
+// class Solution 
+// {
+// public:
+//     // 二分查找
+//     int mySqrt(int x) 
+//     {
+//         // 处理边界情况
+//         if(x < 1)
+//         {
+//             return 0;
+//         }
+        
+//         int left = 1, right = x;
+//         while(left < right)
+//         {
+//             long long mid = left + (right - left + 1) / 2;
+//             if(mid * mid <= x)
+//             {
+//                 left = mid;
+//             }
+//             else
+//             {
+//                 right = mid - 1;
+//             }
+//         }
+        
+//         return left;
+//     }
+// };
