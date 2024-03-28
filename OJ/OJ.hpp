b8a299cb581be39badcd3c6c4000cf10d9b754b9
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-28 23:31:16 +0800

    FindPeakElement

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1713,3 +1713,27 @@ using namespace std;
 //         return left;
 //     }
 // };
+
+// Ѱ�ҷ�ֵ
+// class Solution 
+// {
+// public:
+//     int findPeakElement(vector<int>& nums) 
+//     {
+//         int left = 0, right = nums.size() - 1;
+//         while(left < right)
+//         {
+//             int mid = left + (right - left) / 2;
+//             if(nums[mid] > nums[mid + 1])
+//             {
+//                 right = mid;
+//             }
+//             else
+//             {
+//                 left = mid + 1;
+//             }
+//         }
+    
+//         return left;
+//     }
+// };
