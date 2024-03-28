f27288da360508b9c123d332ba9822d8aaca1086
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-28 22:50:21 +0800

    SearchInsert

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1659,3 +1659,32 @@ using namespace std;
 //         return left;
 //     }
 // };
+
+// ËÑË÷²åÈëÎ»ÖÃ
+class Solution 
+{
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int left = 0, right = nums.size() - 1;
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target)
+            {
+                right = mid;              
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        
+        if(nums[left] < target)
+        {
+            left++;
+        }
+        
+        return left;
+    }
+};
