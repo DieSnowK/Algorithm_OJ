41c819e5bc384f35639df34404c3827194839e3b
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-29 20:43:21 +0800

    FindMin

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1737,3 +1737,28 @@ using namespace std;
 //         return left;
 //     }
 // };
+
+// 寻找旋转排序数组中的最小值
+// class Solution 
+// {
+// public:
+//     int findMin(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         int left = 0, right = n - 1;
+//         while(left < right)
+//         {
+//             int mid = left + (right - left) / 2;
+//             if(nums[mid] > nums[n - 1])
+//             {
+//                 left = mid + 1;
+//             }
+//             else
+//             {
+//                 right = mid;
+//             }
+//         }
+
+//         return nums[left];
+//     }
+// };
