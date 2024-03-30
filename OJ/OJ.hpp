0fafbbd676d4b2c101b68210a2d01e8d3323416d
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-03-30 22:53:59 +0800

    ProductExceptSelf

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -1898,3 +1898,35 @@ using namespace std;
 //         return -1;
 //     }
 // };
+
+// 除自身以外数组的乘积
+// class Solution 
+// {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         vector<int> f(n), g(n);
+//         f[0] = 1, g[n - 1] = 1; // 细节处理
+
+//         // 预处理前缀积数组和后缀积数组
+//         for(int i = 1; i < n; i++)
+//         {
+//             f[i] = f[i - 1] * nums[i - 1];
+//         }
+
+//         for(int i = n - 2; i >= 0; i--)
+//         {
+//             g[i] = g[i + 1] * nums[i + 1];
+//         }
+
+//         // 使用
+//         vector<int> ret(n);
+//         for(int i = 0; i < n; i++)
+//         {
+//             ret[i] = f[i] * g[i];
+//         }
+
+//         return ret;
+//     }
+// };
