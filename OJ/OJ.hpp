0e51dc6d02921563373a9dfee90f7ed23eb0c424
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-01 20:34:55 +0800

    替换所有的问号

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -2207,3 +2207,28 @@ using namespace std;
 //         return {a, b};
 //     }
 // };
+
+// 替换所有的问号
+class Solution 
+{
+public:
+    string modifyString(string s) 
+    {
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '?')
+            {
+                for(char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    // || 短路设计比较精髓
+                    if((i == 0 || ch != s[i - 1]) && (i == n - 1 || ch != s[i + 1]))
+                    {
+                        s[i] = ch;
+                    }
+                }
+            }
+        }
+
+        return s;
+    }
+};
