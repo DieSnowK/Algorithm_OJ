174f341715e26bfcdc835e7664f4e1608e433a05
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-06 16:44:58 +0800

    存在重复元素Ⅱ

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -3400,3 +3400,24 @@ using namespace std;
 //         return false;
 //     }
 // };
+
+// ´æÔÚÖØ¸´ÔªËØ¢ò
+// class Solution 
+// {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) 
+//     {
+//         unordered_map<int, int> hash; // <nums[i], i>
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(hash.count(nums[i]) && i - hash[nums[i]] <= k)
+//             {
+//                 return true;
+//             }
+
+//             hash[nums[i]] = i;
+//         }
+
+//         return false;
+//     }
+// };
