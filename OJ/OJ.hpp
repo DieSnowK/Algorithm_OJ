2936c973eff14d7465129e6ed546115845b1fb82
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-07 22:45:31 +0800

    验证栈序列

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -3806,3 +3806,27 @@ using namespace std;
 //         return strSt.top();
 //     }
 // };
+
+// —È÷§’ª–Ú¡–
+// class Solution 
+// {
+// public:
+//     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
+//     {
+//         stack<int> st;
+
+//         int i = 0;
+//         for(auto& x : pushed)
+//         {
+//             st.push(x);
+
+//             while(st.size() && st.top() == popped[i])
+//             {
+//                 st.pop();
+//                 i++;
+//             }
+//         }
+        
+//         return st.empty();
+//     }
+// };
