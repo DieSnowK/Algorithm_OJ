88ffa959f08fd40bc50bb661c47b716182dc2d42
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-08 12:56:45 +0800

    N叉树的层序遍历

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -3830,3 +3830,66 @@ using namespace std;
 //         return st.empty();
 //     }
 // };
+
+// N叉树的层序遍历
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+// class Solution 
+// {
+// public:
+//     vector<vector<int>> levelOrder(Node* root) 
+//     {
+//         vector<vector<int>> ret; // 记录最终结果
+//         queue<Node*> q; // 层序遍历需要的队列
+        
+//         if(root == nullptr)
+//         {
+//             return ret;
+//         }
+        
+//         q.push(root);
+//         while(q.size())
+//         {
+//             int sz = q.size(); // 提取本层元素个数
+//             vector<int> tmp; // 统计本层元素
+            
+//             while(sz--)
+//             {
+//                 Node* node = q.front();
+//                 q.pop();
+                
+//                 tmp.push_back(node->val);
+                
+//                 // 将该结点下一层入队列
+//                 for(auto& child : node->children)
+//                 {
+//                     if(child != nullptr)
+//                     {
+//                         q.push(child);    
+//                     }
+//                 }
+//             }
+            
+//             ret.push_back(tmp);
+//         }
+        
+//         return ret;
+//     }
+// };
