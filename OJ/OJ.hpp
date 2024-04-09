53bf64c256b747ac0131a9c9c8cf347b1ea3ee7c
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-09 11:17:00 +0800

    数据流的中位数

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -4161,3 +4161,62 @@ public:
 //         return ret;
 //     }
 // };
+
+// 数据流的中位数
+// class MedianFinder 
+// {
+//     priority_queue<int> left; // 大根堆
+//     priority_queue<int, vector<int>, greater<int>> right; // 小根堆
+// public:
+//     MedianFinder() 
+//     {}
+    
+//     void addNum(int num) 
+//     {
+//         if(left.size() == right.size())
+//         {
+//             if(left.empty() || num <= left.top())
+//             {
+//                 left.push(num);
+//             }
+//             else
+//             {
+//                 right.push(num);
+//                 left.push(right.top());
+//                 right.pop();
+//             }
+//         }
+//         else
+//         {
+//             if(num <= left.top())
+//             {
+//                 left.push(num);
+//                 right.push(left.top());
+//                 left.pop();
+//             }
+//             else
+//             {
+//                 right.push(num);
+//             }
+//         }
+//     }
+    
+//     double findMedian() 
+//     {
+//         if(left.size() == right.size())
+//         {
+//             return (left.top() + right.top()) / 2.0;
+//         }
+//         else
+//         {
+//             return left.top();
+//         }
+//     }
+// };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
