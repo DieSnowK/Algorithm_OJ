c2027a2d929a8613a7f00c7aee64e6dad3d676d2
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-10 21:00:02 +0800

    飞地的数量

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -4797,3 +4797,83 @@ public:
 //         return dist;
 //     }
 // };
+
+// 飞地的数量
+// class Solution 
+// {
+//     int dx[4] = {1, -1, 0, 0};
+//     int dy[4] = {0, 0, 1, -1};
+// public:
+//     int numEnclaves(vector<vector<int>>& grid) 
+//     {
+//         int n = grid.size(), m = grid[0].size();
+//         vector<vector<bool>> visit(n, vector<bool>(m, false));
+//         queue<pair<int, int>> q;
+
+//         // 将所有边界1入队列
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(grid[i][0] == 1)
+//             {
+//                 q.push({i, 0});
+//                 visit[i][0] = true;
+//             }
+
+//             if(grid[i][m - 1] == 1)
+//             {
+//                 q.push({i, m - 1});
+//                 visit[i][m - 1] = true;
+//             }
+//         }
+
+//         for(int i = 0; i < m; i++)
+//         {
+//             if(grid[0][i] == 1)
+//             {
+//                 q.push({0, i});
+//                 visit[0][i] = true;
+//             }
+
+//             if(grid[n - 1][i] == 1)
+//             {
+//                 q.push({n - 1, i});
+//                 visit[n - 1][i] = true;
+//             }
+//         }
+
+//         // 多源BFS
+//         while(q.size())
+//         {
+//             auto [a, b] = q.front();
+//             q.pop();
+
+//             // 下一层入队列
+//             for(int i = 0; i < 4; i++)
+//             {
+//                 int x = a + dx[i], y = b + dy[i];
+
+//                 if(x >= 0 && x < n && y >= 0 && y < m \
+//                     && grid[x][y] == 1 && !visit[x][y])
+//                 {
+//                     visit[x][y] = true;
+//                     q.push({x, y});
+//                 }
+//             }
+//         }
+
+//         // 遍历计数
+//         int count = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             for(int j = 0; j < m; j++)
+//             {
+//                 if(grid[i][j] == 1 && !visit[i][j])
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
