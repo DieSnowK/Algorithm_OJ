d7abdab68f511226ee23db02f67653ccc4ed2c91
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-13 14:47:03 +0800

    逆序链表(递归)

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -5262,3 +5262,33 @@ public:
 //         }
 //     }
 // };
+
+// 逆序链表(递归)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution 
+// {
+// public:
+//     ListNode* reverseList(ListNode* head) 
+//     {
+//         // 出口 -> 叶子结点
+//         if(head == nullptr || head->next == nullptr)
+//         {
+//             return head;
+//         }
+
+//         ListNode* newhead = reverseList(head->next);
+//         head->next->next = head;
+//         head->next = nullptr;
+
+//         return newhead;
+//     }
+// };
