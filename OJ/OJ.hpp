2c3ffbf038e76ab0440774ca61eb6a374c1ada64
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-13 21:58:30 +0800

    二叉树剪枝

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -5422,3 +5422,38 @@ public:
 //         return DFS(root->left, prevSum) + DFS(root->right, prevSum);
 //     }
 // };
+
+// ¶þ²æÊ÷¼ôÖ¦
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution 
+// {
+// public:
+//     TreeNode* pruneTree(TreeNode* root) 
+//     {
+//         if(root == nullptr)
+//         {
+//             return nullptr;
+//         }
+
+//         root->left = pruneTree(root->left);
+//         root->right = pruneTree(root->right);
+
+//         if(!root->val && !root->left && !root->right)
+//         {
+//             delete root; // ·ÀÖ¹ÄÚ´æÐ¹Â©
+//             root = nullptr;
+//         }
+
+//         return root;
+//     }
+// };
