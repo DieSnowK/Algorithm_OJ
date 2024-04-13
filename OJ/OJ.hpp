5a74b7f49b1d91aee85cf98426534e21363524ff
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-13 21:10:07 +0800

    求根节点到叶节点数字之和

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -5384,3 +5384,41 @@ public:
 //         }
 //     }
 // };
+
+// 求根节点到叶节点数字之和
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution 
+// {
+// public:
+//     int sumNumbers(TreeNode* root) 
+//     {
+//         return DFS(root, 0);
+//     }
+
+//     int DFS(TreeNode* root, int prevSum)
+//     {
+//         if(root == nullptr)
+//         {
+//             return 0;
+//         }
+
+//         prevSum = prevSum * 10 + root->val;
+
+//         if(root->left == nullptr && root->right == nullptr)
+//         {
+//             return prevSum;
+//         }
+
+//         return DFS(root->left, prevSum) + DFS(root->right, prevSum);
+//     }
+// };
