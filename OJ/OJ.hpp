0a83db03d59518f1c0e62fe3409d445b7cd023bf
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-14 15:37:25 +0800

    全排列

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -5629,3 +5629,41 @@ public:
 //         }
 //     }
 // };
+
+// 全排列
+// class Solution 
+// {
+//     vector<vector<int>> ret;
+//     vector<int> path;
+//     vector<bool> check; // 实现剪枝
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) 
+//     {
+//         check.resize(nums.size(), false);
+//         DFS(nums);
+//         return ret;
+//     }
+
+//     void DFS(vector<int>& nums)
+//     {
+//         if(nums.size() == path.size())
+//         {
+//             ret.push_back(path);
+//             return;
+//         }
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(!check[i])
+//             {
+//                 path.push_back(nums[i]);
+//                 check[i] = true;
+//                 DFS(nums);
+
+//                 // 回溯 -> 恢复现场
+//                 path.pop_back();
+//                 check[i] = false;
+//             }
+//         }
+//     }
+// };
