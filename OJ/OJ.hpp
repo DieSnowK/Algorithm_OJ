32e1738d720a50a2be98b9586f2f4b9bdcc4bd9b
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-14 12:40:38 +0800

    二叉搜索树中第K小的元素

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -5539,3 +5539,45 @@ public:
 //         return left && right && cur;
 //     }
 // };
+
+// 二叉搜索树中第K小的元素
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution 
+// {
+//     int count, ret;
+// public:
+//     int kthSmallest(TreeNode* root, int k) 
+//     {
+//         count = k;
+//         DFS(root);
+//         return ret;
+//     }
+
+//     void DFS(TreeNode* root)
+//     {
+//         // 函数出口 + 剪枝
+//         if(root == nullptr || count == 0)
+//         {
+//             return;
+//         }
+
+//         DFS(root->left);
+
+//         if(--count == 0)
+//         {
+//             ret = root->val;
+//         }
+
+//         DFS(root->right);
+//     }
+// };
