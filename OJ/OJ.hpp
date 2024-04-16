5bcee5a10d2e11082ef7adaecfd8c3fad7994c20
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-16 23:13:07 +0800

    有效的数独

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -6250,3 +6250,35 @@ public:
 //         }
 //     }
 // };
+
+// 有效的数独
+// class Solution 
+// {
+// public:
+//     bool isValidSudoku(vector<vector<char>>& board) 
+//     {
+//         bool row[9][9] = {false};
+//         bool col[9][9] = {false};
+//         bool grid[3][3][9] = {false};
+
+//         for(int i = 0; i < 9; i++)
+//         {
+//             for(int j = 0; j < 9; j++)
+//             {
+//                 if(board[i][j] != '.')
+//                 {
+//                     int num = board[i][j] - 1 - '0';
+//                     if(row[i][num] || col[j][num] || grid[i / 3][j / 3][num])
+//                     {
+//                         return false;
+//                     }
+
+//                     row[i][num] = col[j][num] = grid[i / 3][j / 3][num] = true;
+//                 }
+
+//             }
+//         }
+
+//         return true;
+//     }
+// };
