cff3260836bb7b71695055c645e7281a75d1b6d1
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-19 21:43:40 +0800

    v3.0 动态规划

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7011,3 +7011,25 @@ public:
 //         return mem[i][j];
 //     }
 // };
+
+// v3.0 ¶¯Ì¬¹æ»®
+// int uniquePaths(int n, int m) 
+// {
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     dp[1][1] = 1;
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= m; j++)
+//         {
+//             if(i == 1 && j == 1)
+//             {
+//                 continue;
+//             }
+
+//             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//         }
+//     }
+
+//     return dp[n][m];
+// }
