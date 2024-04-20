62b3c3ca99fa8d53bc9f514149f16dd40850e58c
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-20 19:32:22 +0800

    三步问题

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7316,3 +7316,23 @@ public:
 
 //     return ret;
 // }
+
+// 三步问题
+// int waysToStep(int n) 
+// {
+//     // 边界情况处理
+//     if(n == 1 || n == 2) return n;
+//     if(n == 3) return 4;
+
+//     const int MOD = 1e9 + 7;
+
+//     vector<int> dp(n + 1, 0);
+//     dp[1] = 1, dp[2] = 2, dp[3] = 4;
+
+//     for(int i = 4; i <= n; i++)
+//     {
+//         dp[i] = ((dp[i - 1] + dp[i - 2]) % MOD + dp[i - 3]) % MOD;
+//     }
+
+//     return dp[n];
+// }
