6330d928662829d53aafd4f1ddf978aeb4c8b434
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-20 21:02:00 +0800

    用最小花费爬楼梯 v1.0

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7336,3 +7336,18 @@ public:
 
 //     return dp[n];
 // }
+
+// 使用最小花费爬楼梯
+// v1.0
+// int minCostClimbingStairs(vector<int>& cost) 
+// {
+//     int n = cost.size();
+//     vector<int> dp(n + 1);
+
+//     for(int i = 2; i <= n; i++)
+//     {
+//         dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+//     }
+
+//     return dp[n];
+// }
