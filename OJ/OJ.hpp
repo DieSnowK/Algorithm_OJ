3bd9fa95d1c1f10bc5cf0b04237dcd9f31b99911
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-21 21:03:00 +0800

    不同路径

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7441,3 +7441,20 @@ public:
 
 //     return dp[n];
 // }
+
+// ²»Í¬Â·¾¶
+// int uniquePaths(int n, int m) 
+// {
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     dp[0][1] = 1;
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= m; j++)
+//         {
+//             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//         }
+//     }
+
+//     return dp[n][m];
+// }
