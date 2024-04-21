66c81fbbd4569b4dd100e9a7c381a3e04a7c0a7a
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-21 22:17:36 +0800

    珠宝的最高价值

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7479,3 +7479,20 @@ public:
 
 //     return dp[n][m];
 // }
+
+// 珠宝的最高价值
+// int jewelleryValue(vector<vector<int>>& frame) 
+// {
+//     int n = frame.size(), m = frame[0].size();
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= m; j++)
+//         {
+//             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + frame[i - 1][j - 1];
+//         }
+//     }
+
+//     return dp[n][m];
+// }
