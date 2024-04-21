74a95e0f0a546589f93abed13444494085c9aa13
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-21 21:37:30 +0800

    不同路径Ⅱ

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7458,3 +7458,24 @@ public:
 
 //     return dp[n][m];
 // }
+
+// 不同路径Ⅱ
+// int uniquePathsWithObstacles(vector<vector<int>>& ob) 
+// {
+//     int n = ob.size(), m = ob[0].size();
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     dp[0][1] = 1;
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= m; j++)
+//         {
+//             if(ob[i - 1][j - 1] == 0) // 注意下表映射关系
+//             {
+//                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//             }
+//         }
+//     }
+
+//     return dp[n][m];
+// }
