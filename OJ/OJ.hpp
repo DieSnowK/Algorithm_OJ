1f85acdc9a10d86db3e08ab72428ad38d051a52a
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-24 08:46:10 +0800

    买卖股票的最佳时机含手续费

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7674,3 +7674,20 @@ public:
 
 //     return max(dp[n - 1][1], dp[n - 1][2]);
 // }
+
+// 买卖股票的最佳时机含手续费
+// int maxProfit(vector<int>& prices, int fee) 
+// {
+//     int n = prices.size();
+//     vector<int> f(n); // 买入
+//     vector<int> g(n); // 卖出
+//     f[0] = -prices[0];
+
+//     for(int i = 1; i < n; i++)
+//     {
+//         f[i] = max(f[i - 1], g[i - 1] - prices[i]);
+//         g[i] = max(g[i - 1], f[i - 1] + prices[i] - fee);
+//     }
+
+//     return g[n - 1];
+// }
