41c41c6fac1890b0d5ac62dfe694bbbfc52df60b
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-26 13:54:12 +0800

    乘积为正数的最长子数组长度

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7820,3 +7820,29 @@ public:
 
 //     return ret;
 // }
+
+// 乘积为正数的最长子数组长度
+// int getMaxLen(vector<int>& nums) 
+// {
+//     int n = nums.size();
+//     vector<int> f(n + 1), g(n + 1);
+
+//     int ret = INT_MIN;
+//     for(int i = 1; i <= n; i++)
+//     {
+//         if(nums[i - 1] > 0)
+//         {
+//             f[i] = f[i - 1] + 1;
+//             g[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+//         }
+//         else if(nums[i - 1] < 0)
+//         {
+//             f[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+//             g[i] = f[i - 1] + 1;
+//         }
+
+//         ret = max(ret, f[i]);
+//     }
+
+//     return ret;
+// }
