4ae71fa1cf32600a4ac0e8491259b6eba4765a25
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-26 13:56:52 +0800

    最长湍流子数组

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7862,3 +7862,27 @@ public:
 
 //     return sum;
 // }
+
+// 最长湍流子数组
+// int maxTurbulenceSize(vector<int>& arr) 
+// {
+//     int n = arr.size();
+//     vector<int> f(n, 1), g(n, 1);
+
+//     int ret = 1;
+//     for(int i = 1; i < n; i++)
+//     {
+//         if(arr[i - 1] < arr[i])
+//         {
+//             f[i] = g[i - 1] + 1;
+//         }
+//         else if(arr[i - 1] > arr[i])
+//         {
+//             g[i] = f[i - 1] + 1;
+//         }
+
+//         ret = max(ret, max(f[i], g[i]));
+//     }
+
+//     return ret;
+// }
