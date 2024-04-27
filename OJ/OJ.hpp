1d0b9904e52a4c8671dcceebc974f6471323787d
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-27 20:07:33 +0800

    最长递增子序列的个数

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7999,3 +7999,42 @@ public:
 
 //     return ret;
 // }
+
+// 最长递增子序列的个数
+// int findNumberOfLIS(vector<int>& nums) 
+// {
+//     int n = nums.size();
+//     vector<int> len(n, 1), count(n, 1);
+
+//     int retLen = 1, retCount = 1;
+//     for(int i = 1; i < n; i++)
+//     {
+//         for(int j = 0; j < i; j++)
+//         {
+//             if(nums[j] < nums[i])
+//             {
+//                 if(len[j] + 1 == len[i])
+//                 {
+//                     count[i] += count[j];
+//                 }
+//                 else if(len[j] + 1 > len[i])
+//                 {
+//                     len[i] = len[j] + 1;
+//                     count[i] = count[j];
+//                 }
+//             }
+//         }
+
+//         if(retLen == len[i])
+//         {
+//             retCount += count[i];
+//         }
+//         else if(retLen < len[i])
+//         {
+//             retLen = len[i];
+//             retCount = count[i];
+//         }
+//     }
+
+//     return retCount;
+// }
