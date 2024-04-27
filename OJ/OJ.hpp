3ef7ac883ce76384fb058a8bcde8e7d627c06124
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-27 12:31:18 +0800

    最长递增子序列

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -7949,3 +7949,26 @@ public:
 
 //     return sum;
 // }
+
+// 最长递增子序列
+// int lengthOfLIS(vector<int>& nums) 
+// {
+//     int n = nums.size();
+//     vector<int> dp(n, 1);
+
+//     int ret = 1;
+//     for(int i = 1; i < n; i++)
+//     {
+//         for(int j = 0; j < i; j++)
+//         {
+//             if(nums[j] < nums[i])
+//             {
+//                 dp[i] = max(dp[i], dp[j] + 1);
+//             }
+//         }
+
+//         ret = max(ret, dp[i]);
+//     }
+
+//     return ret;
+// }
