6782265d686d45fa23e7044506999f7da4ffabd6
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-28 19:52:52 +0800

    最长数对链

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8038,3 +8038,28 @@ public:
 
 //     return retCount;
 // }
+
+// 最长数对链
+// int findLongestChain(vector<vector<int>>& pairs) 
+// {
+//     sort(pairs.begin(), pairs.end()); // 预处理
+
+//     int n = pairs.size();
+//     vector<int> dp(n, 1);
+
+//     int ret = 1;
+//     for(int i = 1; i < n; i++)
+//     {
+//         for(int j = 0; j < i; j++)
+//         {
+//             if(pairs[j][1] < pairs[i][0])
+//             {
+//                 dp[i] = max(dp[j] + 1, dp[i]);
+//             }
+//         }
+
+//         ret = max(ret, dp[i]);
+//     }
+
+//     return ret;
+// }
