e7e0db98bec4ff1a616cbe2738c33baaf06fc791
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-04-29 19:11:50 +0800

    最长的斐波那契子序列的长度

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8081,3 +8081,34 @@ public:
 
 //     return ret;
 // }
+
+// 最长的斐波那契子序列的长度
+// int lenLongestFibSubseq(vector<int>& arr) 
+// {
+//     int n = arr.size();
+
+//     // 优化
+//     unordered_map<int, int> hash; // <arr[i], i>
+//     for(int i = 0; i < n; i++)
+//     {
+//         hash[arr[i]] = i;
+//     }
+
+//     vector<vector<int>> dp(n, vector<int>(n, 2));
+
+//     int ret = 2;
+//     for(int j = 2; j < n; j++) // 固定第一个位置
+//     {
+//         for(int i = 1; i < j; i++) // 固定第二个位置
+//         {
+//             int a = arr[j] - arr[i];
+//             if(a < arr[i] && hash.count(a))
+//             {
+//                 dp[i][j] = dp[hash[a]][i] + 1;
+//                 ret = max(ret, dp[i][j]);
+//             }
+//         }
+//     }
+
+//     return ret < 3 ? 0 : ret;
+// }
