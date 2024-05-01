ae255eb923190f20a2c58c79a6029550b78940fe
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-01 13:01:38 +0800

    回文子串

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8178,3 +8178,29 @@ public:
 
 //     return ret;
 // }
+
+// ╩ьндвс╢╝
+// int countSubstrings(string s) 
+// {
+//     int n = s.size();
+//     vector<vector<bool>> dp(n, vector<bool>(n));
+
+//     int ret = 0;
+//     for(int i = n - 1; i >= 0; i--)
+//     {
+//         for(int j = i; j < n; j++)
+//         {
+//             if(s[i] == s[j])
+//             {
+//                 dp[i][j] = i + 1 < j ? dp[i + 1][j - 1] : true;
+
+//                 if(dp[i][j])
+//                 {
+//                     ret++;
+//                 }
+//             }
+//         }
+//     }
+
+//     return ret;
+// }
