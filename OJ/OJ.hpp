c826ad432b3804d9f6cbaf4366ab6690c6e6fc51
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-03 19:39:16 +0800

    最长回文子序列

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8305,3 +8305,28 @@ public:
 
 //     return dp[n - 1];
 // }
+
+// 最长回文子序列
+// int longestPalindromeSubseq(string s) 
+// {
+//     int n = s.size();
+//     vector<vector<int>> dp(n, vector<int>(n));
+
+//     for(int i = n - 1; i >= 0; i--)
+//     {
+//         dp[i][i] = 1;
+//         for(int j = i + 1; j < n; j++)
+//         {
+//             if(s[i] == s[j])
+//             {
+//                 dp[i][j] = dp[i + 1][j - 1] + 2;
+//             }
+//             else
+//             {
+//                 dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+//             }
+//         }
+//     }
+
+//     return dp[0][n - 1];
+// }
