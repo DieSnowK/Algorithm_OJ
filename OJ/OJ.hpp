a1653d51d8559e2d52eb1a671ad1d612729578f3
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-05 21:13:57 +0800

    交错字符串

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8511,3 +8511,51 @@ public:
 
 //     return dp[n][m];
 // }
+
+// 交错字符串
+// bool isInterleave(string s1, string s2, string s3) 
+// {
+//     int n = s1.size(), m = s2.size();
+//     if(n + m != s3.size()) return false;
+
+//     s1 = " " + s1, s2 = " " + s2, s3 = " " + s3;
+//     vector<vector<bool>> dp(n + 1, vector<bool>(m + 1));
+
+//     // Init
+//     dp[0][0] = true;
+//     for(int i = 1; i <= m; i++) // 第一行
+//     {
+//         if(s2[i] == s3[i])
+//         {
+//             dp[0][i] = true;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+
+//     for(int i = 1; i <= n; i++) // 第一列
+//     {
+//         if(s1[i] == s3[i])
+//         {
+//             dp[i][0] = true;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+
+//     // DP
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= m; j++)
+//         {
+//             dp[i][j] = (s1[i] == s3[i + j] && dp[i - 1][j])
+//                 || (s2[j] == s3[i + j] && dp[i][j - 1]);
+//         }
+//     }
+
+//     return dp[n][m];
+// }
