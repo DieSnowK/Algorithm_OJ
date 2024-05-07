baf28faafc144550bc3e2a5c20a29644f25901c1
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-07 10:32:41 +0800

    v2.0 滚动数组优化

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -8840,28 +8840,51 @@ public:
 
 // 最后一块石头的重量 II
 // v1.0
-int lastStoneWeightII(vector<int>& stones) 
-{
-    int sum = 0;
-    for(auto& x : stones)
-    {
-        sum += x;
-    }
+// int lastStoneWeightII(vector<int>& stones) 
+// {
+//     int sum = 0;
+//     for(auto& x : stones)
+//     {
+//         sum += x;
+//     }
 
-    int n = stones.size(), m = sum / 2;
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+//     int n = stones.size(), m = sum / 2;
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1));
 
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 0; j <= m; j++)
-        {
-            dp[i][j] = dp[i - 1][j];
-            if(j >= stones[i - 1])
-            {
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
-            }
-        }
-    }
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 0; j <= m; j++)
+//         {
+//             dp[i][j] = dp[i - 1][j];
+//             if(j >= stones[i - 1])
+//             {
+//                 dp[i][j] = max(dp[i][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
+//             }
+//         }
+//     }
 
-    return sum - 2 * dp[n][m];
-}
+//     return sum - 2 * dp[n][m];
+// }
+
+// v2.0 滚动数组优化
+// int lastStoneWeightII(vector<int>& stones) 
+// {
+//     int sum = 0;
+//     for(auto& x : stones)
+//     {
+//         sum += x;
+//     }
+
+//     int n = stones.size(), m = sum / 2;
+//     vector<int> dp(m + 1);
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = m; j >= stones[i - 1]; j--)
+//         {
+//             dp[j] = max(dp[j], dp[j - stones[i - 1]] + stones[i - 1]);
+//         }
+//     }
+
+//     return sum - 2 * dp[m];
+// }
