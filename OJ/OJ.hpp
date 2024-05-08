b1ca011bb9deac55e89abc9501500594559002c5
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-08 12:41:18 +0800

    零钱兑换Ⅱ 滚动数组版本

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -9038,3 +9038,20 @@ public:
 
 //     return dp[amount] >= INF ? -1 : dp[amount];
 // }
+
+// ¡„«Æ∂“ªª¢Ú
+// int change(int amount, vector<int>& coins) 
+// {
+//     vector<int> dp(amount + 1);
+//     dp[0] = 1;
+
+//     for(int i = 1; i <= coins.size(); i++)
+//     {
+//         for(int j = coins[i - 1]; j <= amount; j++)
+//         {
+//             dp[j] += dp[j - coins[i - 1]];
+//         }
+//     }
+
+//     return dp[amount];
+// }
