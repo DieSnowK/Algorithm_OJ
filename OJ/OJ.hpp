6d43a83756d6da1fabb1e71574a41b4bd1a5a36c
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-10 22:27:11 +0800

    不同的二叉搜索树

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -9251,3 +9251,20 @@ public:
 
 //     return dp[target];
 // }
+
+// 不同的二叉搜索树
+// int numTrees(int n) 
+// {
+//     vector<int> dp(n + 1);
+//     dp[0] = 1;
+
+//     for(int i = 1; i <= n; i++)
+//     {
+//         for(int j = 1; j <= i; j++)
+//         {
+//             dp[i] += dp[i - j] * dp[j - 1];
+//         }
+//     }
+
+//     return dp[n];
+// }
