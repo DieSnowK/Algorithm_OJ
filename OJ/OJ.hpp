e27d06d2c7e636c96fc6988d6452673d54b8f15a
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-11 20:26:58 +0800

    柠檬水找零

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -9268,3 +9268,46 @@ public:
 
 //     return dp[n];
 // }
+
+// ÄûÃÊË®ÕÒÁã
+// bool lemonadeChange(vector<int>& bills) 
+// {
+//     int five = 0, ten = 0;
+//     for(auto& x : bills)
+//     {
+//         if(x == 5)
+//         {
+//             five++;
+//         }
+//         else if(x == 10)
+//         {
+//             if(five == 0) 
+//             {
+//                 return false;
+//             }
+//             else
+//             {
+//                 five--;
+//                 ten++;
+//             }
+//         }
+//         else
+//         {
+//             if(ten && five)
+//             {
+//                 ten--;
+//                 five --;
+//             }
+//             else if(five >= 3)
+//             {
+//                 five -= 3;
+//             }
+//             else
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     return true;
+// }
