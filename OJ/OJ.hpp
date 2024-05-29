a074ed55cf8f379b801273bccbbe7c7f7e91e7b2
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-29 08:46:27 +0800

    最优除法

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -9757,3 +9757,26 @@ public:
 
 //     return ret;
 // }
+
+// ×îÓÅ³ı·¨
+// string optimalDivision(vector<int>& nums) 
+// {
+//     int n = nums.size();
+//     if(n == 1)
+//     {
+//         return to_string(nums[0]);
+//     }
+//     if(n == 2)
+//     {
+//         return to_string(nums[0]) + "/" + to_string(nums[1]);
+//     }
+
+//     string ret = to_string(nums[0]) + "/(" + to_string(nums[1]);
+//     for(int i = 2; i < n; i++)
+//     {
+//         ret += "/" + to_string(nums[i]);
+//     }
+//     ret += ")";
+
+//     return ret;
+// }
