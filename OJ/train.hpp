5b4bd3aa8f76c82a9e97a1aea30bd4e4f63a1d04
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-07-15 00:19:43 +0800

    数组中两个字符串的最小距离

--- OJ/train.hpp
+++ OJ/train.hpp
@@ -82,3 +82,75 @@ using namespace std;
 
 //     return 0;
 // }
+
+// 数组中两个字符串的最小距离
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <cstdlib>
+// #include <climits>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 0;
+//     string str1, str2;
+//     cin >> n >> str1 >> str2;
+
+//     vector<string> strs(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> strs[i];
+//     }
+
+//     if (str1.empty() && str2.empty())
+//     {
+//         cout << -1 << endl;
+//         return 0;
+//     }
+
+//     // 双指针
+//     int p1 = 0, p2 = 0, ret1 = 0, ret2 = 0, minDist = INT_MAX;
+//     bool flag1 = false, flag2 = false;
+
+//     while (p1 < n || p2 < n)
+//     {
+//         // 找str1
+//         while (p1 < n)
+//         {
+//             if (strs[p1] == str1)
+//             {
+//                 flag1 = true;
+//                 ret1 = p1++;
+//                 break;
+//             }
+
+//             p1++;
+//         }
+//         if (flag1 && flag2)
+//         {
+//             minDist = min(minDist, abs(ret1 - ret2));
+//         }
+
+//         // 找str2
+//         while (p2 < n)
+//         {
+//             if (strs[p2] == str2)
+//             {
+//                 flag2 = true;
+//                 ret2 = p2++;
+//                 break;
+//             }
+
+//             p2++;
+//         }
+//         if (flag1 && flag2)
+//         {
+//             minDist = min(minDist, abs(ret1 - ret2));
+//         }
+//     }
+
+//     cout << (minDist == INT_MAX ? -1 : minDist) << endl;
+
+//     return 0;
+// }
