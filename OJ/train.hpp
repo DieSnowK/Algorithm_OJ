45afb1e56ccc8b183ee1582a9fc33398086957dd
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-08-08 02:41:43 +0800

    礼物的最大价值

--- OJ/train.hpp
+++ OJ/train.hpp
@@ -792,3 +792,20 @@ using namespace std;
 
 //     return 0;
 // }
+
+// 礼物的最大价值
+// int maxValue(vector<vector<int>> &grid)
+// {
+//     int n = grid.size(), m = grid[0].size();
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= m; j++)
+//         {
+//             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+//         }
+//     }
+
+//     return dp[n][m];
+// }
