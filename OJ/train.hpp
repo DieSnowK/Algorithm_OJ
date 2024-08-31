594f9efa342f9976e525f3bb71cf99d434c9207a
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-09-01 00:07:02 +0800

    计算字符串的编辑距离

--- OJ/train.hpp
+++ OJ/train.hpp
@@ -1720,3 +1720,47 @@ using namespace std;
 
 //     return 0;
 // }
+
+// ¼ÆËã×Ö·û´®µÄ±à¼­¾àÀë
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     string a, b;
+//     cin >> a >> b;
+//     int n = a.size(), m = b.size();
+
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     for (int j = 0; j <= m; j++)
+//     {
+//         dp[0][j] = j;
+//     }
+
+//     for (int i = 0; i <= n; i++)
+//     {
+//         dp[i][0] = i;
+//     }
+
+//     // DP
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= m; j++)
+//         {
+//             if (a[i - 1] == b[j - 1])
+//             {
+//                 dp[i][j] = dp[i - 1][j - 1];
+//             }
+//             else
+//             {
+//                 dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+//             }
+//         }
+//     }
+
+//     cout << dp[n][m] << endl;
+
+//     return 0;
+// }
