87f038f477739ec2deb71622be641b24493a2dd8
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-11-22 22:48:30 +0800

    俄罗斯套娃 方法二

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -10084,3 +10084,44 @@ public:
 
 //     return ret;
 // }
+
+// 俄罗斯套娃 方法二
+// int maxEnvelopes(vector<vector<int>> &e)
+// {
+//     // 重写排序
+//     sort(e.begin(), e.end(), [&](const vector<int> &v1, const vector<int> &v2)
+//          { return v1[0] != v2[0] ? v1[0] < v2[0] : v1[1] > v2[1]; });
+
+//     // 贪心 + 二分
+//     vector<int> ret;
+//     ret.push_back(e[0][1]);
+
+//     for (int i = 1; i < e.size(); i++)
+//     {
+//         int b = e[i][1];
+//         if (b > ret.back())
+//         {
+//             ret.push_back(b);
+//         }
+//         else
+//         {
+//             int left = 0, right = ret.size() - 1;
+//             while (left < right)
+//             {
+//                 int mid = (left + right) / 2;
+//                 if (ret[mid] >= b)
+//                 {
+//                     right = mid;
+//                 }
+//                 else
+//                 {
+//                     left = mid + 1;
+//                 }
+//             }
+
+//             ret[left] = b;
+//         }
+//     }
+
+//     return ret.size();
+// }
