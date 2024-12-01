a4597c296a2489c154eb72b72c9c84819d863276
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-12-01 12:41:58 +0800

    距离相等的条形码

--- OJ/OJ.hpp
+++ OJ/OJ.hpp
@@ -10176,3 +10176,47 @@ public:
 //         return max(sum - y1, sum - x1 - x2);
 //     }
 // }
+
+// 距离相等的条形码
+// vector<int> rearrangeBarcodes(vector<int> &b)
+// {
+//     unordered_map<int, int> hash;
+//     int maxVal = 0, maxCount = 0;
+//     for (auto x : b)
+//     {
+//         if (maxCount < ++hash[x])
+//         {
+//             maxCount = hash[x];
+//             maxVal = x;
+//         }
+//     }
+
+//     int n = b.size();
+//     int index = 0;
+//     vector<int> ret(n);
+
+//     // 先处理出现次数最多的那个数
+//     for (int i = 0; i < maxCount; i++)
+//     {
+//         ret[index] = maxVal;
+//         index += 2;
+//     }
+
+//     // 处理剩下的数
+//     hash.erase(maxVal);
+//     for (auto &[x, y] : hash)
+//     {
+//         for (int i = 0; i < y; i++)
+//         {
+//             if (index >= n)
+//             {
+//                 index = 1;
+//             }
+
+//             ret[index] = x;
+//             index += 2;
+//         }
+//     }
+
+//     return ret;
+// }
