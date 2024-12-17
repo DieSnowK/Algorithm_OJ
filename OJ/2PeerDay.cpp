0037d23c7bbe84070f48f4453b74417c104d7b52
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-12-18 00:15:59 +0800

    前缀和

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -22,3 +22,37 @@
 //         return {-1, -1};
 //     }
 // };
+
+// 前缀和
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 0, q = 0;
+//     cin >> n >> q;
+
+//     vector<int> nums(n + 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cin >> nums[i];
+//     }
+
+//     // 预处理出前缀和数组
+//     vector<long long> dp(n + 1, 0);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         dp[i] = dp[i - 1] + nums[i];
+//     }
+
+//     // 使用预处理数组
+//     int l = 0, r = 0;
+//     while (q--)
+//     {
+//         cin >> l >> r;
+//         cout << dp[r] - dp[l - 1] << endl;
+//     }
+
+//     return 0;
+// }
