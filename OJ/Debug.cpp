256a5aac7278342f065bab3dd647ff9d68528be5
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-12-21 13:00:11 +0800

    Update

--- OJ/Debug.cpp
+++ OJ/Debug.cpp
@@ -1,80 +1,40 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
-int date = 0, year = 0;
-int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-bool isLeap(int year)
+bool Check(int x)
 {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
-
-bool Check(int x) // x == year
-{
-    string str = to_string(x);
-    reverse(str.begin(), str.end());
-
-    int _month = atoi(str.substr(0, 2).c_str());
-    int _day = atoi(str.substr(2, 2).c_str());
-
-    if (isLeap(x) && _month == 2 && _day > 29)
+    int cnt = 1;
+    while (x)
     {
-        return false;
-    }
+        int tmp = x % 10; // 拿个位
+        x /= 10;
 
-    if (_month > 12 || _day > days[_month])
-    {
-        return false;
+        // 奇数位, 但不是奇数 || 偶数位, 但不是偶数
+        if ((cnt % 2 == 1) && (tmp % 2 != 1) || (cnt % 2 == 0) && (tmp % 2 != 0))
+        {
+            return false;
+        }
+
+        cnt++;
     }
 
     return true;
 }
 
-bool CheckAB(int x)
-{
-    /*
-    return (x / 1000 == (x % 100) / 10) &&
-           ((x % 1000) / 100 == x % 10) &&
-           (x / 1000 != (x % 1000) / 100);
-    */
-
-    string str = to_string(x);
-    return str[0] == str[2] && str[1] == str[3] && str[0] != str[1]; // ABABBABA
-}
-
-// 枚举每个回文, 然后判断是否是合法日期即可
 int main()
 {
-    cin >> date;
-    year = date / 10000;
+    int n = 0, ret = 0;
+    cin >> n;
 
-    // 1.枚举回文
-    bool found = false;
-    string tmp, ret;
-    for (int i = year + 1; i < 10000; i++)
+    for (int i = 1; i <= n; i++)
     {
-        // 2.是否是合法日期?
         if (Check(i))
         {
-            tmp = to_string(i);
-            ret = tmp + string(tmp.rbegin(), tmp.rend());
-
-            if (!found)
-            {
-                cout << ret << endl;
-                found = true;
-            }
-
-            // 3.是回文, 是否是ABABBABA?
-            if (CheckAB(i))
-            {
-                cout << ret << endl;
-                break;
-            }
+            ret++;
         }
     }
+
+    cout << ret;
 
     return 0;
 }
