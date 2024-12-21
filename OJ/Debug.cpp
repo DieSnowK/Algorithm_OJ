63e725728b2882482a09b5b8df3cf141fc7cbad7
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-12-21 15:05:40 +0800

    Update

--- OJ/Debug.cpp
+++ OJ/Debug.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-using namespace std;
-
-bool Check(int x)
-{
-    int cnt = 1;
-    while (x)
-    {
-        int tmp = x % 10; // 拿个位
-        x /= 10;
-
-        // 奇数位, 但不是奇数 || 偶数位, 但不是偶数
-        if ((cnt % 2 == 1) && (tmp % 2 != 1) || (cnt % 2 == 0) && (tmp % 2 != 0))
-        {
-            return false;
-        }
-
-        cnt++;
-    }
-
-    return true;
-}
-
-int main()
-{
-    int n = 0, ret = 0;
-    cin >> n;
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (Check(i))
-        {
-            ret++;
-        }
-    }
-
-    cout << ret;
-
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//bool Check(int x)
+//{
+//    int cnt = 1;
+//    while (x)
+//    {
+//        int tmp = x % 10; // 拿个位
+//        x /= 10;
+//
+//        // 奇数位, 但不是奇数 || 偶数位, 但不是偶数
+//        if ((cnt % 2 == 1) && (tmp % 2 != 1) || (cnt % 2 == 0) && (tmp % 2 != 0))
+//        {
+//            return false;
+//        }
+//
+//        cnt++;
+//    }
+//
+//    return true;
+//}
+//
+//int main()
+//{
+//    int n = 0, ret = 0;
+//    cin >> n;
+//
+//    for (int i = 1; i <= n; i++)
+//    {
+//        if (Check(i))
+//        {
+//            ret++;
+//        }
+//    }
+//
+//    cout << ret;
+//
+//    return 0;
+//}
