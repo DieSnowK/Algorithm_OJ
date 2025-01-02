412d8225c33ee62f5c6f714df70ee83be6c71cb0
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2025-01-03 00:29:35 +0800

    最大子数组和

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -245,3 +245,25 @@
 //         return newHead;
 //     }
 // };
+
+// 最大子数组和
+// class Solution
+// {
+// public:
+//     // dp[i] : 以i位置元素为结尾的所有子数组中的最大和
+//     int maxSubArray(vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         vector<int> dp(n + 1, 0);
+//         dp[0] = 0;
+
+//         int ret = -0x3f3f3f3f;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             dp[i] = max(dp[i - 1] + nums[i - 1], nums[i - 1]);
+//             ret = max(ret, dp[i]);
+//         }
+
+//         return ret;
+//     }
+// };
