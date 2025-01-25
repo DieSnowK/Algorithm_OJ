8b1c5805513952a710fefe8f95a5fd000f99ca49
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2025-01-26 01:06:55 +0800

    二位前缀和

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -521,3 +521,45 @@
 //         return i + 1;
 //     }
 // };
+
+// 二位前缀和
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 0, m = 0, q = 0;
+//     cin >> n >> m >> q;
+
+//     vector<vector<int>> nums(n + 1, vector<int>(m + 1, 0));
+//     for(int i = 1; i <= n; ++i)
+//     {
+//         for(int j = 1; j <= m; ++j)
+//         {
+//             cin >> nums[i][j];
+//         }
+//     }
+
+//     // dp[i][j]: dp[1][1] -- dp[i][j]的和
+//     vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+//     for(int i = 1; i <= n; ++i)
+//     {
+//         for(int j = 1; j <= m; ++j)
+//         {
+//             dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + nums[i][j];
+//         }
+//     }
+
+//     // 使用前缀和数组
+//     int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+//     long long ret = 0;
+//     while(q--)
+//     {
+//         cin >> x1 >> y1 >> x2 >> y2;
+//         ret = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+//         cout << ret << endl;
+//     }
+
+//     return 0;
+// }
