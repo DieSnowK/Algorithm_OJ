e487d6db71dae63583e7117aec0a0ecfca5a7985
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2025-01-30 07:31:45 +0800

    最小K个数

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -625,3 +625,14 @@
 //         return root;
 //     }
 // };
+
+// 最小K个数
+// class Solution 
+// {
+// public:
+    // vector<int> smallestK(vector<int>& arr, int k) 
+    // {
+        // sort(arr.begin(), arr.end());
+        // return vector<int>(arr.begin(), arr.begin() + k);
+    // }
+// };
