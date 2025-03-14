42ca1d7f5c01fb331ae2e843821a07b257f08ff8
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2025-03-14 09:28:53 +0800

    第三大的数 -- 排序

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -1743,3 +1743,24 @@
 //         return ret;
 //     }
 // };
+
+// 第三大的数 -- 排序
+// class Solution 
+// {
+// public:
+//     int thirdMax(vector<int>& nums) 
+//     {
+//         sort(nums.begin(), nums.end(), greater<>());
+
+//         int diff = 1;
+//         for(int i = 1; i < nums.size(); i++)
+//         {
+//             if(nums[i] != nums[i - 1] && ++diff == 3)
+//             {
+//                 return nums[i];
+//             }
+//         }
+
+//         return nums[0];
+//     }
+// };
