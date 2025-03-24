9dda50c4d7cdfc325e455207f62b6344e9dcd764
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2025-03-25 07:16:46 +0800

    数组中两元素的最大乘积

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -2087,3 +2087,14 @@
 //         return ret;
 //     }
 // };
+
+// 数组中两元素的最大乘积
+// class Solution
+// {
+// public:
+//     int maxProduct(vector<int> &nums)
+//     {
+//         sort(nums.begin(), nums.end(), greater<>());
+//         return (nums[0] - 1) * (nums[1] - 1);
+//     }
+// };
