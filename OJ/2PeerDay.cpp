6e6f4ea26775d0432781244a0b5aa2a0584613b7
DieSnowK <1752351098@qq.com>
2025-05-21 21:51:39 +0800

    格雷编码

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -3698,3 +3698,18 @@
 //         }
 //     }
 // };
+
+// 格雷编码
+// class Solution 
+// {
+// public:
+//     vector<int> grayCode(int n)
+//     {
+//         vector<int> ret(1 << n);
+//         for (int i = 0; i < ret.size(); i++)
+//         {
+//             ret[i] = (i >> 1) ^ i;
+//         }
+//         return ret;
+//     }
+// };
