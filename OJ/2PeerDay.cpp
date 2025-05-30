d0f33931abdf44c81db66c3ccbaaf4802906716d
DieSnowK <1752351098@qq.com>
2025-05-30 10:41:54 +0800

    判定是否互为字符重排 --> 排序

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -4009,3 +4009,21 @@
 //         return ret;
 //     }
 // };
+
+// 判定是否互为字符重排 --> 排序
+// class Solution 
+// {
+// public:
+//     bool CheckPermutation(string s1, string s2) 
+//     {
+//         if (s1.length() != s2.length()) 
+//         {
+//             return false;
+//         }
+
+//         sort(s1.begin(), s1.end());
+//         sort(s2.begin(), s2.end());
+        
+//         return s1 == s2;
+//     }
+// };
