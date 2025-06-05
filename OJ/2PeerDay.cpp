19516bf276dd5f8f8120e2f20a51b6af93a964a5
DieSnowK <1752351098@qq.com>
2025-06-06 07:48:34 +0800

    翻转二叉树

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -4246,3 +4246,23 @@
 //         return dp[n];
 //     }
 // };
+
+// 翻转二叉树
+// class Solution 
+// {
+// public:
+//     TreeNode* invertTree(TreeNode* root) 
+//     {
+//         if (root == nullptr) 
+//         {
+//             return nullptr;
+//         }
+
+//         TreeNode* left = invertTree(root->left);
+//         TreeNode* right = invertTree(root->right);
+//         root->left = right;
+//         root->right = left;
+        
+//         return root;
+//     }
+// };
