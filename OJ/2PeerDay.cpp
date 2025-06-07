caa7f9b365bd83a455bab2fd3195677f588568c3
DieSnowK <1752351098@qq.com>
2025-06-07 11:15:11 +0800

    杨辉三角

--- OJ/2PeerDay.cpp
+++ OJ/2PeerDay.cpp
@@ -4266,3 +4266,25 @@
 //         return root;
 //     }
 // };
+
+// 杨辉三角
+// class Solution 
+// {
+// public:
+//     vector<vector<int>> generate(int numRows) 
+//     {
+//         vector<vector<int>> triangle(numRows);
+
+//         for(int i = 0; i < numRows; i++)
+//         {
+//             triangle[i].resize(i + 1, 1);
+
+//             for(int j = 1; j < i; j++)
+//             {
+//                 triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+//             }
+//         }
+
+//         return triangle;
+//     }
+// };
